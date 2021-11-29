c5f83dd91077fa4e4b88be617ddabeec07515b66
Aaron <ken888686@gmail.com>
2021-11-29 14:15:58 +0800

    add update products list button

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -1,17 +1,20 @@
 <template>
   <section class="wrap productDisplay">
-    <select name="" class="productSelect" @change="onChange">
-      <option value="全部">全部</option>
-      <option value="床架">床架</option>
-      <option value="收納">收納</option>
-      <option value="窗簾">窗簾</option>
-    </select>
+    <div class="d-flex align-items-center mb-2">
+      <select name="" class="productSelect mb-0 me-3" @change="onChange">
+        <option value="全部">全部</option>
+        <option value="床架">床架</option>
+        <option value="收納">收納</option>
+        <option value="窗簾">窗簾</option>
+      </select>
+      <button type="button" class="btn btn-outline-dark" @click="getProducts">更新產品列表</button>
+    </div>
     <ul class="productWrap">
       <Loading :active="isLoading" />
       <li class="productCard" v-for="item in products" :key="item.id">
         <h4 class="productType">新品</h4>
         <img :src="item.images" :alt="item.description" />
-        <a href="#" class="addCardBtn">加入購物車</a>
+        <a href="#" class="addCardBtn" @click.prevent="addToCart(item.id)">加入購物車</a>
         <h3>{{ item.title }}</h3>
         <del class="originPrice">NT${{ item.origin_price }}</del>
         <p class="nowPrice">NT${{ item.price }}</p>
@@ -47,6 +50,9 @@ export default {
         this.allProducts = res.data.products;
         this.isLoading = false;
       });
+    },
+    addToCart(id) {
+      console.log(id);
     },
   },
   mounted() {
