9241a3f06352c13194e5d479587dc5604a52ac3c
ken888686@gmail.com <asas456789>
2021-11-29 23:45:51 +0800

    update

--- src/components/OrderInfo.vue
+++ src/components/OrderInfo.vue
@@ -2,7 +2,7 @@
   <section class="orderInfo wrap" id="orderInfo">
     <h3 class="section-title">填寫預訂資料</h3>
     <form action="" class="orderInfo-form">
-      <div class="orderInfo-formGroup">
+      <div class="orderInfo-formGroup" :class="{ error: v$.customerName.$errors.length }">
         <label for="customerName" class="orderInfo-label">姓名</label>
         <div class="orderInfo-inputWrap">
           <input
@@ -11,8 +11,16 @@
             id="customerName"
             placeholder="請輸入姓名"
             name="姓名"
+            v-model="customerName"
           />
-          <p class="orderInfo-message" data-message="姓名">必填</p>
+          <p
+            class="orderInfo-message"
+            data-message="姓名"
+            v-for="error of v$.customerName.$errors"
+            :key="error.$uid"
+          >
+            必填
+          </p>
         </div>
       </div>
       <div class="orderInfo-formGroup">
@@ -24,6 +32,7 @@
             id="customerPhone"
             placeholder="請輸入電話"
             name="電話"
+            v-model="customerPhone"
           />
           <p class="orderInfo-message" data-message="電話">必填</p>
         </div>
@@ -37,6 +46,7 @@
             id="customerEmail"
             placeholder="請輸入 Email"
             name="Email"
+            v-model="customerEmail"
           />
           <p class="orderInfo-message" data-message="Email">必填</p>
         </div>
@@ -50,6 +60,7 @@
             id="customerAddress"
             placeholder="請輸入寄送地址"
             name="寄送地址"
+            v-model="customerAddress"
           />
           <p class="orderInfo-message" data-message="寄送地址">必填</p>
         </div>
@@ -57,14 +68,54 @@
       <div class="orderInfo-formGroup">
         <label for="tradeWay" class="orderInfo-label">交易方式</label>
         <div class="orderInfo-inputWrap">
-          <select id="tradeWay" class="orderInfo-input" name="交易方式">
-            <option value="ATM" selected>ATM</option>
+          <select id="tradeWay" class="orderInfo-input" name="交易方式" v-model="payment">
+            <option value="ATM">ATM</option>
             <option value="信用卡">信用卡</option>
             <option value="超商付款">超商付款</option>
           </select>
         </div>
       </div>
-      <input type="submit" value="送出預訂資料" class="orderInfo-btn" />
+      <input type="submit" value="送出預訂資料" class="orderInfo-btn" @click.prevent="submit" />
     </form>
   </section>
 </template>
+<script>
+import useVuelidate from '@vuelidate/core';
+import { required, email } from '@vuelidate/validators';
+import * as service from '@/services';
+
+export default {
+  setup() {
+    return { v$: useVuelidate() };
+  },
+  data() {
+    return {
+      customerName: '',
+      customerPhone: '',
+      customerEmail: '',
+      customerAddress: '',
+      payment: 'ATM',
+    };
+  },
+  validations() {
+    return {
+      customerName: { required },
+      customerPhone: { required },
+      customerEmail: { required, email },
+      customerAddress: { required },
+    };
+  },
+  methods: {
+    submit() {
+      console.log(
+        this.customerName,
+        this.customerPhone,
+        this.customerEmail,
+        this.customerAddress,
+        this.payment,
+      );
+    },
+  },
+  mounted() {},
+};
+</script>
